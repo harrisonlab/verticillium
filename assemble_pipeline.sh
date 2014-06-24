604e33dfccf35d7eee014c5d58410ce2b6f002c6
Richard Harrison <richard.harrison@emr.ac.uk>
2014-06-24 12:13:22 -0700

    Bugfish in PATH

--- assemble_pipeline.sh
+++ assemble_pipeline.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # A script to view the number of genes, signal peptides and effectors identified by path_pipe	
 GENOMES=$1
-PATH=$2
 LIST=`tail -n +2 $GENOMES |cut -d, -f4`
+PATHDATA=$2
 
-echo "Assembling wilt genome from the wilt.csv file"
-cd $PATH
+
+echo "Assembling wilt genome from the $GENOMES file"
+cd $PATHDATA
 
 for ASSEMBLY in $LIST; do
-echo "Assembling "
-echo $ASSEMBLY
-cd $ASSEMBLY
-cat ./F/*.fastq>R1.fastq
-cat ./R/*.fastq>R2.fastq
-echo "Working directory" 
-echo $PATH
-#SPAdes_assemble_part1.sh ./F/R1.fastq ./R/R2.fastq ./assembly/prog1/V.dahliae/$ASSEMBLY/
+echo "Assembling $ASSEMBLY "
 
-R1=$1
-R2=$2
-WORK_DIR=$3
+echo "Working directory" 
+echo $PATHDATA
+
+#cd $ASSEMBLY
+#cat ./F/*.fastq>R1.fastq
+#cat ./R/*.fastq>R2.fastq
+
+#SPAdes_assemble_part1.sh ./F/R1.fastq ./R/R2.fastq ./assembly/prog1/V.dahliae/$ASSEMBLY/
 
 
 done;
